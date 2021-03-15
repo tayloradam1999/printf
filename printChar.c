52e5c167b7dd3eed503867f6cd360e3087956ff0
RLewis11769 <2708@holbertonschool.com>
2021-03-15 17:38:27 +0000

    Add 0/NULL edge cases

--- printChar.c
+++ printChar.c
@@ -9,8 +9,14 @@
 
 int printChar(va_list arg_list, int count)
 {
-	_putchar(va_arg(arg_list, int) + '\0');
-	count++;
+	char c = va_arg(arg_list, int);
+
+	if (c != '\0')
+	{
+		_putchar(c);
+		count++;
+	}
+
 	return (count);
 }
 
@@ -23,9 +29,13 @@ int printChar(va_list arg_list, int count)
 
 int printPercent(va_list arg_list, int count)
 {
-	(void)arg_list;
-	_putchar('%');
-	count++;
+	int p = va_arg(arg_list, int);
+
+	if (p != '\0')
+	{
+		_putchar('%');
+		count++;
+	}
 	return (count);
 }
 
@@ -38,10 +48,14 @@ int printPercent(va_list arg_list, int count)
 
 int printString(va_list arg_list, int count)
 {
+	int x;
+
 	/* Puts given string into variable my_string */
 	char *my_string = va_arg(arg_list, char*);
 
-	int x;
+	if (my_string == NULL)
+		my_string = "(null)";
+
 	/* Iterates through string and prints every character */
 	for (x = 0; my_string[x] != '\0'; x++)
 	{
